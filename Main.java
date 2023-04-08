9f7dfe0f65bf8bbeb07aa7c885bb334de782cd03
Juan Cotera <51240536+juancotera@users.noreply.github.com>
2023-04-08 03:54:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+//EJRCICIO 1
+if (numero < 0) {
+    System.out.println("El número es negativo");
+} else if (numero > 0) {
+    System.out.println("El número es positivo");
+} else {
+    System.out.println("El número es cero");
+}
+
+//EJERCICIO 2
+int numeroWhile = 0;
+while (numeroWhile < 3) {
+    numeroWhile++;
+    System.out.println(numeroWhile);
+}
